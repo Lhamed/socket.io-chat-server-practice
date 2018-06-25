28d3f08b7ee3060fe44b597a5c6cbe8ba31cf124
Unknown <dbswo320@catholic.ac.kr>
2018-06-25 21:11:08 +0900

    LocalWell

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-'use strict';
+
 var http = require('http');
-var port = process.env.PORT || 1337;
+var port =  1337;
+var io = require('socket.io')({
+    transports: ['websocket'],
+});
 
 const server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port);
 
-var io = require('socket.io');
-var socket = io.listen(server);
 
-socket.on('connection', function (client) {
+
+io.attach(server, {
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
+
+
+io.on('connection', function (client) {
     client.on('join', function (data) {
         console.log(data.user + ' : ' + data.roomname);
 
@@ -23,7 +32,7 @@ socket.on('connection', function (client) {
     client.on('chat', function (data) {
         console.log(data.user + ' : ' + data.msg + ' : ' + data.date);
 
-        socket.sockets.in(client.room).emit('chat', data);
+        io.sockets.in(client.room).emit('chat', data);
     });
 
     client.on('disconnect', function () {
