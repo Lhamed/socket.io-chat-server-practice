310165e5274397d505867599a885bf6f8d65a08b
Unknown <dbswo320@catholic.ac.kr>
2018-06-25 18:22:45 +0900

    aaa

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,27 +1,29 @@
-var io = require('socket.io')({
-    transports: ['websocket'],
-});
 
 
+var path = require('path');
 
-io.attach( 1337);
+exports.startup = function (context, done) {
+    var io = require('socket.io')(context.app.server);
+    io.attach(1337);
+    io.on('connection', function (socket) {
+        socket.on('join', function (data) {
+            console.log(data.user + ' : ' + data.roomname);
 
-io.on('connection', function (socket) {
-    socket.on('join', function (data) {
-        console.log(data.user + ' : ' + data.roomname);
+            socket.leave(socket.room);
+            socket.join(data.roomname);
+            socket.room = data.roomname;
+            console.log('己傍利栏肺 立加');
+        });
 
-        socket.leave(socket.room);
-        socket.join(data.roomname);
-        socket.room = data.roomname;
-        console.log('己傍利栏肺 立加');
-    });
+        socket.on('chat', function (data) {
+            console.log(data.user + ' : ' + data.msg + ' : ' + data.date);
 
-    socket.on('chat', function (data) {
-        console.log(data.user + ' : ' + data.msg + ' : ' + data.date);
+            io.sockets.in(socket.room).emit('chat', data);
+        });
 
-        io.sockets.in(socket.room).emit('chat', data);
-    });
-
-    socket.on('disconnect', function () {
-    });
-})
+        socket.on('disconnect', function () {
+        });
+    })
+ 
+    done();
+}
