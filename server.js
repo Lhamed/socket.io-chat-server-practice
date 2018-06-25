ca97a7079919a2c35253172aebbd4428f71e77b0
Unknown <dbswo320@catholic.ac.kr>
2018-06-25 19:18:45 +0900

    daadadad1

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 
+var http = require('http');
+var io = require('socket.io');
+var port = process.env.port || 1337;
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World 2n');
+}).listen(port);
 
-var path = require('path');
+var socket = io.listen(server);
+1  io.on('connection', function (client) {
+    client.on('join', function (data) {
+        console.log(data.user + ' : ' + data.roomname);
 
-exports.startup = function (context, done) {
-    var io = require('socket.io')(context.app.server);
-    io.attach(process.env.PORT || 1337);
-    io.on('connection', function (socket) {
-        socket.on('join', function (data) {
-            console.log(data.user + ' : ' + data.roomname);
+        client.leave(client.room);
+        client.join(data.roomname);
+        client.room = data.roomname;
+        console.log('己傍利栏肺 立加');
+    });
 
-            socket.leave(socket.room);
-            socket.join(data.roomname);
-            socket.room = data.roomname;
-            console.log('己傍利栏肺 立加');
-        });
+    client.on('chat', function (data) {
+        console.log(data.user + ' : ' + data.msg + ' : ' + data.date);
 
-        socket.on('chat', function (data) {
-            console.log(data.user + ' : ' + data.msg + ' : ' + data.date);
+        socket.sockets.in(client.room).emit('chat', data);
+    });
 
-            io.sockets.in(socket.room).emit('chat', data);
-        });
+    client.on('disconnect', function () {
+    });
+})
 
-        socket.on('disconnect', function () {
-        });
-    })
- 
-    done();
-}
