a856ba1deff041bf2bae94191ab81088b7f623d4
Unknown <dbswo320@catholic.ac.kr>
2018-06-25 17:35:55 +0900

    first

---
+++ NodejsWebApp1/server.js
@@ -0,0 +1,27 @@
+var io = require('socket.io')({
+    transports: ['websocket'],
+});
+
+
+
+io.attach(1337);
+
+io.on('connection', function (socket) {
+    socket.on('join', function (data) {
+        console.log(data.user + ' : ' + data.roomname);
+
+        socket.leave(socket.room);
+        socket.join(data.roomname);
+        socket.room = data.roomname;
+        console.log('己傍利栏肺 立加');
+    });
+
+    socket.on('chat', function (data) {
+        console.log(data.user + ' : ' + data.msg + ' : ' + data.date);
+
+        io.sockets.in(socket.room).emit('chat', data);
+    });
+
+    socket.on('disconnect', function () {
+    });
+})
