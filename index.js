fb1c3ed749c3aa41562bdfa09e02ac6cfa20c3a6
Unknown <dbswo320@catholic.ac.kr>
2018-06-26 15:35:28 +0900

    dssds
    sdsd

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+var io = require('socket.io')(server);
+
+io.attach(server, {
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
+
+
+io.on('connection', function (client) {
+    client.on('join', function (data) {
+        console.log(data.user + ' : ' + data.roomname);
+
+        client.leave(client.room);
+        client.join(data.roomname);
+        client.room = data.roomname;
+        console.log('己傍利栏肺 立加');
+    });
+
+    client.on('chat', function (data) {
+        console.log(data.user + ' : ' + data.msg + ' : ' + data.date);
+
+        io.sockets.in(client.room).emit('chat', data);
+    });
+
+    client.on('disconnect', function () {
+    });
+})
