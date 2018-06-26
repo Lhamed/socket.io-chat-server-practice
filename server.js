655519b668c9782d72398a085dddef8c4331273c
Unknown <dbswo320@catholic.ac.kr>
2018-06-26 12:13:17 +0900

    S erver local well now
    gfgf

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 
-var http = require('http');
-var port = process.env.PORT || 80;
-var io = require('socket.io')({
-    transports: ['websocket'],
-});
 
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var app = require('express')();
+app.set('port', process.env.PORT || 80);
+var server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
+});
+var io = require('socket.io')(server);
+
+//var http = require('http');
+//var port = process.env.PORT || 80;
+//var io = require('socket.io')({
+//    transports: ['websocket'],
+//});
+
+//const server = http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
 
 
 
