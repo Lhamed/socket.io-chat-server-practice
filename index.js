b9456954b8668ce52d196ffc05cf6fea4f34b835
Unknown <dbswo320@catholic.ac.kr>
2018-06-28 23:16:23 +0900

    Ha..

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
  var http = require('http');
   var io = require('socket.io');
   var port = process.env.port || 1337;
@@ -25,3 +26,47 @@
       client.on('disconnect', function () {
       });
   })
+=======
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+var io = require('socket.io')(server, { transports: ['websocket'] });
+
+//io.attach(server, {
+//    pingInterval: 10000,
+//    pingTimeout: 5000,
+//    cookie: false
+//});
+
+
+io.on('connection', function (client) {
+    console.log('己傍利栏肺 目池记');
+    client.on('join', function (data) {
+        console.log(data.user + ' : ' + data.roomname);
+
+        client.leave(client.room);
+        client.join(data.roomname);
+        client.room = data.roomname;
+        console.log('己傍利栏肺 立加');
+    });
+
+    client.on('chat', function (data) {
+        console.log(data.user + ' : ' + data.msg + ' : ' + data.date);
+
+        io.sockets.in(client.room).emit('chat', data);
+    });
+
+    client.on('disconnect', function () {
+    });
+})
+>>>>>>> 9cb9606... Ha..
