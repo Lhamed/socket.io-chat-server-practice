cc131c64e77c358612728d45b09bfc39d8b27e31
Unknown <dbswo320@catholic.ac.kr>
2018-06-28 23:18:04 +0900

    Hi I'm renew

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-<<<<<<< HEAD
- var http = require('http');
-  var io = require('socket.io');
-  var port = process.env.port || 1337;
-  var server = http.createServer(function (req, res) {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello World 2n');
-  }).listen(port);
 
-  var socket = io.listen(server); 
-  socket.on('connection', function (client) {
-      client.on('join', function (data) {
-          console.log(data.user + ' : ' + data.roomname);
-          client.leave(client.room);
-          client.join(data.roomname);
-          client.room = data.roomname;
-
-      });
-
-      client.on('chat', function (data) {
-          console.log(data.user + ' : ' + data.msg + ' : ' + data.date);
-
-          socket.sockets.in(client.room).emit('chat', data);
-      });
-
-      client.on('disconnect', function () {
-      });
-  })
-=======
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
@@ -50,14 +22,14 @@ var io = require('socket.io')(server, { transports: ['websocket'] });
 
 
 io.on('connection', function (client) {
-    console.log('己傍利栏肺 目池记');
+console.log('connection');
     client.on('join', function (data) {
         console.log(data.user + ' : ' + data.roomname);
 
         client.leave(client.room);
         client.join(data.roomname);
         client.room = data.roomname;
-        console.log('己傍利栏肺 立加');
+
     });
 
     client.on('chat', function (data) {
@@ -69,4 +41,3 @@ io.on('connection', function (client) {
     client.on('disconnect', function () {
     });
 })
->>>>>>> 9cb9606... Ha..
