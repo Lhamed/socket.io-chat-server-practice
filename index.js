b13e8f9aeb10809680265c77ade81622aab18f42
Unknown <dbswo320@catholic.ac.kr>
2018-06-29 15:38:45 +0900

    BattelPixel

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-
 var http = require('http');
-
 var server = http.createServer(function(request, response) {
-
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-
 });
-
 var port = process.env.PORT || 1337;
 server.listen(port);
-
 console.log("Server running at http://localhost:%d", port);
-var io = require('socket.io')(server, { transports: ['websocket'] });
-
-//io.attach(server, {
-//    pingInterval: 10000,
-//    pingTimeout: 5000,
-//    cookie: false
-//});
-
-
+var io = require('socket.io')(server, { transports: ['websocket'], });
 io.on('connection', function (client) {
 console.log('connection');
     client.on('join', function (data) {
@@ -29,7 +15,7 @@ console.log('connection');
         client.leave(client.room);
         client.join(data.roomname);
         client.room = data.roomname;
-
+        io.sockets.in(client.room).emit('join', data);
     });
 
     client.on('chat', function (data) {
@@ -38,6 +24,11 @@ console.log('connection');
         io.sockets.in(client.room).emit('chat', data);
     });
 
+    client.on('move', function (data) {
+        io.sockets.in(client.room).emit('move', data);
+    });
+
     client.on('disconnect', function () {
+        io.sockets.in(client.room).emit('dis');
     });
 })
